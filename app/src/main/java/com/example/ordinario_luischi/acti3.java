76e741a6529fa07dfea79a5f2a6f2e96f085a976
luischi14 <luis34chigmail.com>
2022-04-28 13:17:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ordinario_luischi/acti3.java
@@ -0,0 +1,10 @@
+package apptiva.arequipa.huaca;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class ContactoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto);
+    }
+}
